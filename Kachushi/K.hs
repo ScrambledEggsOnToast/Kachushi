ebccaa623bd031ef00d226db1f679daa97221281
joshkirklin <tekn04321@gmail.com>
2014-06-23 03:01:31 +0100

    Miner changes clothes

--- Kachushi/K.hs
+++ Kachushi/K.hs
@@ -10,11 +10,5 @@ import Kachushi.OFCP
 
 type K = StateT KState IO
 
-instance MonadRandom K where
-    getRandom = lift getRandom
-    getRandomR = lift . getRandomR
-    getRandoms = lift getRandoms
-    getRandomRs = lift . getRandomRs
-
 displayBoard :: K ()
 displayBoard = do s <- get; liftIO $ printBoard . view board $ s
